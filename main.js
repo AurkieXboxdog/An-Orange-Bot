5a90ce3e9caa1be65fac2ad27f9ed8b72171731f
Aurick <aurick.shi@gmail.com>
2022-05-25 22:17:08 +0930

    part 2

--- main.js
+++ main.js
@@ -11,10 +11,13 @@ require("dotenv").config()
 
 const prefix = '-'
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [ 
         "GUILDS", 
         "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ],
 });
 
@@ -28,6 +31,16 @@ client.on("messageCreate", (message) => {
     } else if (message.content == "ping"){
         message.reply("pong")
     }
+})
+
+const welcomeChannelId = "977464931530178610"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(TOKEN)
